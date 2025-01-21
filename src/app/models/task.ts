fdde40a9321ef6f09dfd56c8b350a2688e220ed9
Dayane Santos <daysantos@deloitte.com>
2025-01-21 14:29:01 -0300

    Criação de componentes e serviço

---
+++ src/app/models/task.ts
@@ -0,0 +1,11 @@
+export class Task {
+    description: string;
+    edition: boolean;
+
+    constructor(d: string, e:boolean){
+        this.description = d;
+        this.edition = e;
+    }
+
+
+}
